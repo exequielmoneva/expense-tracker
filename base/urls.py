52b82ac2b32ed4077896b47124fec3d1ce15a82e
exequielmoneva <exequielmoneva@gmail.com>
2022-04-09 11:40:18 -0300

    CRUD fully functional

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import ExpenseList, ExpenseDetail
+from .views import (
+    ExpenseList,
+    ExpenseDetail,
+    ExpenseCreate,
+    ExpenseUpdate,
+    ExpenseDelete,
+)
 
 urlpatterns = [
     path("", ExpenseList.as_view(), name="expenses"),
     path("expense/<int:pk>", ExpenseDetail.as_view(), name="expense"),
+    path("expense-create/", ExpenseCreate.as_view(), name="expense-create"),
+    path("expense-update/<int:pk>", ExpenseUpdate.as_view(), name="expense-update"),
+    path("expense-delete/<int:pk>", ExpenseDelete.as_view(), name="expense-delete"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
-# Create your views here.
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from base.models import Expenses
 
 
@@ -14,3 +14,22 @@ class ExpenseDetail(DetailView):
     model = Expenses
     context_object_name = "expense"
     template_name = "base/expense.html"
+
+
+class ExpenseCreate(CreateView):
+    model = Expenses
+    # fields = ['title', 'description', 'total_amount', 'original_currency', 'final_currency']
+    fields = "__all__"
+    success_url = reverse_lazy("expenses")
+
+
+class ExpenseUpdate(UpdateView):
+    model = Expenses
+    fields = "__all__"
+    success_url = reverse_lazy("expenses")
+
+
+class ExpenseDelete(DeleteView):
+    model = Expenses
+    context_object_name = "expense"
+    success_url = reverse_lazy("expenses")
